bf1ef4dc5d7b85973f9f31008a9404878337ff12
rohan <purohit.rohan@gmail.com>
2020-03-22 14:47:47 +0530

    Add more device key actions
    - Screenshot
    - Ringer modes
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I23cbdc892b319764c8f8ff49951c823a66f18c28

--- sdk/src/java/org/lineageos/internal/util/DeviceKeysConstants.java
+++ sdk/src/java/org/lineageos/internal/util/DeviceKeysConstants.java
@@ -26,7 +26,9 @@ public class DeviceKeysConstants {
         LAST_APP,
         SPLIT_SCREEN,
         KILL_APP,
-        PLAY_PAUSE_MUSIC;
+        PLAY_PAUSE_MUSIC,
+        SCREENSHOT,
+        RINGER_MODES;
 
         public static Action fromIntSafe(int id) {
             if (id < NOTHING.ordinal() || id > Action.values().length) {
