d1f1b9927a9f0e784e38bdaa91c417479671d9e1
grepfox <grepfox@tutamail.com>
2026-03-04 17:42:55 +0530

    sdk: Bring back Portrait battery icon [2/3]
    Change-Id: Iea8325d5aece691f9564581ddd5b22ccad9070ee

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -983,17 +983,18 @@ public final class LineageSettings {
 
         /**
          * Display style of the status bar battery information
-         * 0: Display the battery an icon in portrait mode
+         * 0: Display the battery as an icon in landscape mode
          * 1: Display the battery as a circle
          * 2: Display the battery as plain text
          * 3: Display the battery as a dotted circle
+         * 4: Display the battery as an icon in portrait mode
          * default: 0
          */
         public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
 
         /** @hide */
         public static final Validator STATUS_BAR_BATTERY_STYLE_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 3);
+                new InclusiveIntegerRangeValidator(0, 4);
 
         /**
          * Status bar battery %
